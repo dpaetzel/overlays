8fea0719d965e778a016f092279ccebb6a9534b8
David Pätzel <david.paetzel@posteo.de>
2021-12-14 12:25:54 +0100

    Fix non-composability of Python overlays

--- flake.nix
+++ flake.nix
@@ -1,67 +1,84 @@
 {
-  description = "A collection of useful overlays";
+  description = "A collection of useful flakes";
 
-  outputs = { self, ... }: {
+  outputs = { self, ... }: rec {
+    # https://github.com/NixOS/nixpkgs/issues/44426
+    # https://discourse.nixos.org/t/makeextensibleasoverlay/7116/5
+    pythonPackageOverlay = attr: overlay: self: super: {
+      ${attr} = self.lib.fix (py:
+        super.${attr}.override (old: {
+          self = py;
+          packageOverrides =
+            self.lib.composeExtensions (old.packageOverrides or (_: _: { }))
+            overlay;
+        }));
+    };
     overlays = {
-      pandas = final: prev: {
-        python3 = prev.python3.override {
-          packageOverrides = python-final: python-prev: {
-            pandas = python-prev.pandas.overrideAttrs (attrs: rec {
-              pname = "pandas";
-              version = "1.3.4";
+      pandas = pythonPackageOverlay "python3" (final: prev: {
+          pandas = prev.pandas.overridePythonAttrs (attrs: rec {
+            pname = "pandas";
+            version = "1.3.4";
 
-              src = python-prev.fetchPypi {
-                inherit pname version;
-                sha256 = "1z3gm521wpm3j13rwhlb4f2x0645zvxkgxij37i3imdpy39iiam2";
-              };
-            });
-          };
-        };
-      };
-      mlflow = final: prev: {
-        python3 = prev.python3.override {
-          packageOverrides = python-final: python-prev: {
-            sqlalchemy = python-prev.sqlalchemy.overrideAttrs (attrs: rec {
-              pname = "SQLAlchemy";
-              version = "1.3.13";
-              src = python-prev.fetchPypi {
-                inherit pname version;
-                sha256 =
-                  "sha256:1yxlswgb3h15ra8849vx2a4kp80jza9hk0lngs026r6v8qcbg9v4";
-              };
-              doInstallCheck = false;
-            });
-            alembic = python-prev.alembic.overrideAttrs (attrs: rec {
-              pname = "alembic";
-              version = "1.4.1";
-              src = python-prev.fetchPypi {
-                inherit pname version;
-                sha256 =
-                  "sha256:0a4hzn76csgbf1px4f5vfm256byvjrqkgi9869nkcjrwjn35c6kr";
-              };
-              propagatedBuildInputs = with python-prev; [
-                python-editor
-                python-dateutil
-                python-final.sqlalchemy
-                Mako
-              ];
-              doInstallCheck = false;
-            });
-            mlflow = (python-prev.mlflow.override {
-              sqlalchemy = python-final.sqlalchemy;
-              alembic = python-final.alembic;
-            }).overrideAttrs (attrs: {
-              propagatedBuildInputs = attrs.propagatedBuildInputs
-                ++ (with python-final; [
-                  importlib-metadata
-                  prometheus-flask-exporter
-                  azure-storage-blob
-                ]);
-              meta.broken = false;
-            });
-          };
-        };
-      };
+            src = prev.fetchPypi {
+              inherit pname version;
+              sha256 = "1z3gm521wpm3j13rwhlb4f2x0645zvxkgxij37i3imdpy39iiam2";
+            };
+          });
+        });
+      mlflow = pythonPackageOverlay "python3" (final: prev: {
+          sqlalchemy = prev.sqlalchemy.overridePythonAttrs (attrs: rec {
+            pname = "SQLAlchemy";
+            # Version 1.3.13 seems to be incompatible with Python 3.9.
+            # version = "1.3.13";
+            version = "1.4.0";
+            src = prev.fetchPypi {
+              inherit pname version;
+              sha256 =
+                "sha256-nP7yrTDF7h1JTZjzxVqawp7G0pS3CEnFQdE55P4adOY=";
+            };
+            doInstallCheck = false;
+          });
+          alembic = prev.alembic.overridePythonAttrs (attrs: rec {
+            pname = "alembic";
+            version = "1.4.1";
+            src = prev.fetchPypi {
+              inherit pname version;
+              sha256 =
+                "sha256:0a4hzn76csgbf1px4f5vfm256byvjrqkgi9869nkcjrwjn35c6kr";
+            };
+            # Something is broken in the alembic tests (probably has to do with
+            # some incompatibility with the SQLAlchemy version):
+            # AttributeError: 'PytestFixtureFunctions' object has no attribute
+            # 'mark_base_test_class'.
+            doCheck = false;
+            propagatedBuildInputs = with prev; [
+              python-editor
+              python-dateutil
+              final.sqlalchemy
+              Mako
+            ];
+            doInstallCheck = false;
+          });
+          mlflow = (prev.mlflow.override {
+            sqlalchemy = final.sqlalchemy;
+            alembic = final.alembic;
+            pandas = final.pandas;
+          }).overridePythonAttrs (attrs: rec {
+            pname = "mlflow";
+            version = "1.22.0";
+            src = prev.fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-9oA5BxXkNq44z3BW7JEDD8nrZ8xjEibyj/lQT745Wt0=";
+            };
+            propagatedBuildInputs = attrs.propagatedBuildInputs
+              ++ (with final; [
+                importlib-metadata
+                prometheus-flask-exporter
+                azure-storage-blob
+              ]);
+            meta.broken = false;
+          });
+        });
     };
   };
 }
